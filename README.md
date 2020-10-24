20c8845ec1e5526a8d95802fb33e7cbc56583345
ML Horizons <themlhorizons@gmail.com>
2020-10-24 17:21:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Deploying Intent Classifier on Heroku
+
+Code for the medium blog post [Deploying Intent Classifier on Heroku]().
+
+## Local Deployment
+### Requirements
+
+Install the required libraries using
+
+`pip install -r requirements.txt`
+
+### Training
+For code to the training files, check [here](https://github.com/horizons-ml/intent-classifier).
+You can find the tutorial for training the Intent Classifier, check [here](https://medium.com/analytics-vidhya/creating-your-own-intent-classifier-b86e000a4926).
+
+### Inference
+Run the server using
+
+`python app.py`
+
+Then go this url
+http://localhost:5000
+
+### Screenshot
+![Screenshot](https://github.com/horizons-ml/heroku-intent-classifier-deployment/raw/main/images/screenshot.PNG)
+
+### License
+Please check `LICENSE` for more details.
---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template
+from flask import request
+
+import pickle
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+class IntentClassifier:
+    def __init__(self,classes,model,tokenizer,label_encoder):
+        self.classes = classes
+        self.classifier = model
+        self.tokenizer = tokenizer
+        self.label_encoder = label_encoder
+
+    def get_intent(self,text):
+        self.text = [text]
+        self.test_keras = self.tokenizer.texts_to_sequences(self.text)
+        self.test_keras_sequence = pad_sequences(self.test_keras, maxlen=16, padding='post')
+        self.pred = self.classifier.predict(self.test_keras_sequence)
+        return self.label_encoder.inverse_transform(np.argmax(self.pred,1))[0]
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  
+    if request.method == 'POST':
+      form = request.form
+    
+      model = load_model('models/intents.h5')
+        
+      with open('utils/classes.pkl','rb') as file:
+        classes = pickle.load(file)
+
+      with open('utils/tokenizer.pkl','rb') as file:
+        tokenizer = pickle.load(file)
+
+      with open('utils/label_encoder.pkl','rb') as file:
+        label_encoder = pickle.load(file)
+
+      nlu = IntentClassifier(classes,model,tokenizer,label_encoder)  
+        
+      result = []
+      sentence = form['sentence']
+      prediction = nlu.get_intent(sentence)
+
+      result.append(form['sentence'])
+      result.append(prediction)
+
+      return render_template("index.html",result = result)
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
