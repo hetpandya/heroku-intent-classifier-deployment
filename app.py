661fe1a48dc73cd9b4879a6b749050ee507b4c08
ML Horizons <themlhorizons@gmail.com>
2020-10-24 18:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import pickle
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-from tensorflow.python import keras
-
-keras.backend.clear_session()
+import tensorflow as tf
 
 class IntentClassifier:
     def __init__(self,classes,model,tokenizer,label_encoder):
@@ -21,30 +19,33 @@ class IntentClassifier:
         self.text = [text]
         self.test_keras = self.tokenizer.texts_to_sequences(self.text)
         self.test_keras_sequence = pad_sequences(self.test_keras, maxlen=16, padding='post')
-        self.pred = self.classifier.predict(self.test_keras_sequence)
+        wtih graph.as_default():
+            self.pred = self.classifier.predict(self.test_keras_sequence)
         return self.label_encoder.inverse_transform(np.argmax(self.pred,1))[0]
 
 app = Flask(__name__)
+
+global graph
+
+model = load_model('models/intents.h5')
+graph = tf.get_default_graph()
+with open('utils/classes.pkl','rb') as file:
+    classes = pickle.load(file)
+
+with open('utils/tokenizer.pkl','rb') as file:
+    tokenizer = pickle.load(file)
+
+with open('utils/label_encoder.pkl','rb') as file:
+    label_encoder = pickle.load(file)
+
+      nlu = IntentClassifier(classes,model,tokenizer,label_encoder) 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   
     if request.method == 'POST':
-      form = request.form
-    
-      model = load_model('models/intents.h5')
-        
-      with open('utils/classes.pkl','rb') as file:
-        classes = pickle.load(file)
-
-      with open('utils/tokenizer.pkl','rb') as file:
-        tokenizer = pickle.load(file)
-
-      with open('utils/label_encoder.pkl','rb') as file:
-        label_encoder = pickle.load(file)
-
-      nlu = IntentClassifier(classes,model,tokenizer,label_encoder)  
+      form = request.form 
         
       result = []
       sentence = form['sentence']
